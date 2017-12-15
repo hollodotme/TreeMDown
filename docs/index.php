462c4118c70b68963e0176d540fc8502730ffe27
Holger Woltersdorf <hw@hollo.me>
2017-12-15 15:49:15 +0100

    Adds namespace

--- docs/index.php
+++ docs/index.php
@@ -4,12 +4,14 @@
  * @author hollodotme
  */
 
+namespace hollodotme\TreeMDown;
+
 error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$tmd = new \hollodotme\TreeMDown\TreeMDown( __DIR__ . '/TreeMDown' );
+$tmd = new TreeMDown( __DIR__ . '/TreeMDown' );
 $tmd->setDefaultFile( '01-What-Is-TreeMDown.md' );
 $tmd->enablePrettyNames();
 $tmd->hideFilenameSuffix();
