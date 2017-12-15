a73533823816b846025442466deb8f23e6de54eb
Holger Woltersdorf <hw@hollo.me>
2017-12-15 15:48:51 +0100

    Updates js/css

--- src/Assets/js/treemdown.js
+++ src/Assets/js/treemdown.js
@@ -4,14 +4,15 @@
  * url: https://github.com/hollootme/TreeMDown
  */
 
-$(document).ready(function () {
-    $('.tmd-folder-link').click(function (event) {
-        var toggle_id = $(this).data('subtree-id');
-        $('#' + toggle_id).toggle();
-    });
+$( document ).ready( function () {
+	$( '.tmd-folder-link' ).click( function ( event ) {
+		var toggle_id = $( this ).data( 'subtree-id' );
+		$( '#' + toggle_id ).toggle();
+	} );
 
-    $('#reset-main-search').click(function (event) {
-        $('#main-search').val('');
-        this.form.submit();
-    });
-});
+	$( '#reset-main-search' ).click( function ( event ) {
+		$( '#main-search' ).val( '' );
+		this.form.submit();
+	} );
+	$( 'img' ).addClass( 'img-responsive' );
+} );
